07829fa656666316a337a76843262d464a3929e4
hoangphuoc912 <hoangphuoctm3@gmail.com>
2026-03-12 23:47:53 +0700

    Upload source code

---
+++ Slide1/src/slide1/Dog.java
@@ -0,0 +1,11 @@
+package slide1;
+
+public class Dog extends Animal{
+	public Dog(String name, int age) {
+		super(name,age);
+	}
+	
+	public void bark() {
+		System.out.println(name + " sủa: Gau Gau");
+	}
+}
---
+++ Slide1/src/slide1/MayTinh.java
@@ -0,0 +1,13 @@
+package slide1;
+
+public class MayTinh {
+	public int tinhTong(int a, int b) {
+		return a + b;
+	}
+	public double tinhTong(double a, int b) {
+		return a + b;
+	}
+	public int tinhTong (int a, int b , int c ) {
+		return a + b + c;
+	}
+}
